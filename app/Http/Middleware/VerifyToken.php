104b3667c618ae71dc5b142e5c1f5fd41503fc35
agi <muhammadagiandi@gmail.com>
2023-05-08 07:23:01 -0700

    first commit

---
+++ app/Http/Middleware/VerifyToken.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Token;
+
+class VerifyToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            //code...
+            $cookie = request()->cookie('X-AUTH-TOKEN');
+            $token = new Token($cookie);
+            JWTAuth::decode($token);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return redirect('/logout');
+        }
+        return $next($request);
+    }
+}
